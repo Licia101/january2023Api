76437af507de2b96b8cda2fd69dc576be1edd135
licia101 <sebualice1@gmail.com>
2023-02-17 23:52:02 +0100

    Added changes to socialNetworkingPost class

--- src/test/java/restAssurredDemo/SocialNetworkingPost.java
+++ src/test/java/restAssurredDemo/SocialNetworkingPost.java
@@ -29,4 +29,24 @@ public class SocialNetworkingPost {
                 then().statusCode(201).log().all().
                 body("title", is("My last holiday in japan"));
     }
+    @Test
+    public void getSpecificSocialNetworkingPostCopy() {
+        given().contentType(ContentType.JSON).log().all().
+                when().get("https://jsonplaceholder.typicode.com/posts/6").
+                then().statusCode(200).log().all().
+                body("title", is("dolorem eum magni eos aperiam quia")).
+                body("body", is("ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"));
+    }
+    @Test
+    public void createASocialNetworkingPostCopy(){
+        HashMap<String, String> PostRequestBody = new HashMap<>();
+        PostRequestBody.put("userId", "1");
+        PostRequestBody.put("title", "My last holiday in japan");
+        PostRequestBody.put("body", "I went holiday with my family , we had a good time. It was in the winter");
+
+        given().contentType(ContentType.JSON).log().all().with().body(PostRequestBody).
+                when().post("https://jsonplaceholder.typicode.com/posts").
+                then().statusCode(201).log().all().
+                body("title", is("My last holiday in japan"));
+    }
 }
